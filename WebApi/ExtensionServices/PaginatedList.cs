481acb20f70991e72679d863ebec1cad880b55ee
涛 徐 <xt2271754098@gmail.com>
2020-03-19 20:54:28 +0800

    PaginatedList

---
+++ WebApi/ExtensionServices/PaginatedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.ExtensionServices
+{
+    public class PaginatedList<T>:List<T> where T :class
+    {
+
+        public PaginationBase paginationBase { get; set; }
+
+    }
+}
---
+++ WebApi/ExtensionServices/PaginationBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.ExtensionServices
+{
+    public class PaginationBase
+    {
+        //默认分页大小
+        private int _PageSize = 10;
+
+        //分页最大值
+        private int MaxPageSize = 100;
+
+        public int PageIndex { get; set; } = 0;
+
+        public int PageSize
+        {
+            get => _PageSize;
+            set => _PageSize = value > MaxPageSize ? MaxPageSize : _PageSize;
+        }
+    }
+}
